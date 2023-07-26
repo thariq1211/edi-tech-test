db96d2307243a8ae456beb2664b9fc1f9e5d91b9
m.thariq <m.thariq@majoo.id>
2023-07-26 09:10:04 +0700

    initial app commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,117 +1,111 @@
+'use client'
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import * as React from 'react';
 
-const inter = Inter({ subsets: ['latin'] })
+const className_td = { className: 'uppercase p-2' };
+
+const Dialog = ({ open, onOpenChange }) => {
+  const initial = { name: '', username: '', passw: '', status: false }
+  const [user, setUser] = React.useState(initial);
+  const [errorMsg, setErroMsg] = React.useState('');
+  React.useEffect(() => {
+    if (!open) setUser(initial);
+  }, [open])
+  const onsubmit = async (e) => {
+    e.preventDefault();
+    setErroMsg('');
+    try {
+      const response = await fetch('/api/users', { method: 'POST', body: JSON.stringify(user) })
+      const json = response.json();
+      if (json) onOpenChange();
+    } catch (error) {
+      setErroMsg(error);
+    }
+  }
+  if (open) return (
+    <div className='bg-amber-400 z-10 absolute top-auto bottom-auto left-auto right-auto h-fit min-w-[600px] rounded-xl shadow-sm shadow-amber-300 p-4'>
+      <div className='flex h-5 w-full justify-between text-gray-800'>
+        <h1 className='text-lg font-semibold'>Tambah User</h1>
+        <button onClick={onOpenChange} className='rounded-full mr-2 text-xl'>x</button>
+      </div>
+      <div className='mt-8 text-gray-800'>
+        <form onSubmit={onsubmit} className='flex flex-col gap-1 text-sm'>
+          <label className='grid grid-cols-2' for="nama_lengkap">
+            <span>Nama Lengkap</span>
+            <input onChange={({ target }) => setUser(p => ({ ...p, name: target.value }))} className='outline-none p-1 rounded-sm text-slate-950 text-sm placeholder:text-slate-950 placeholder:text-sm' id="nama_lengkap" type="text" placeholder='Masukkan Nama Lengkap' />
+          </label>
+          <label className='grid grid-cols-2' for="username">
+            <span>Username</span>
+            <input onChange={({ target }) => setUser(p => ({ ...p, username: target.value }))} className='outline-none p-1 rounded-sm text-slate-950 text-sm placeholder:text-slate-950 placeholder:text-sm' id="username" type="text" placeholder='Masukkan Username' />
+          </label>
+          <label className='grid grid-cols-2' for="password">
+            <span>Password</span>
+            <input onChange={({ target }) => setUser(p => ({ ...p, passw: target.value }))} className='outline-none p-1 rounded-sm text-slate-950 text-sm placeholder:text-slate-950 placeholder:text-sm' id="password" type="password" placeholder='Masukkan Password' />
+          </label>
+          <label className='grid grid-cols-2' for="status">
+            <span>Status</span>
+            <input onChange={({ target }) => setUser(p => ({ ...p, status: target.checked }))} className='p-1 rounded-sm text-slate-950 text-sm placeholder:text-slate-950 placeholder:text-sm' id="status" type="checkbox"/>
+          </label>
+          {errorMsg !== '' && <span className='text-pink-700 text-lg'>{JSON.stringify(errorMsg)}</span>}
+          <button className='mt-[25px] rounded-md text-sm py-2 px-3 w-fit bg-red-500 text-slate-50' type="submit">Submit</button>
+        </form>
+      </div>
+    </div>
+  )
+  return null;
+}
 
 export default function Home() {
+  const [dialogOpen, setDialog] = React.useState(false);
+  const [data, setData] = React.useState([]);
+  const fetch = async () => {
+    try {
+      const response = await fetch('/api/users')
+      const json = response.json();
+      if (json) setData(json);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  React.useEffect(fetch, []);
+  const deleteRecord = async (id) => {
+      try {
+        const response = await fetch('/api/users', { method: "DELETE", body: JSON.stringify({ id }) })
+        const json = response.json();
+        if (json)  fetch();
+      } catch (error) {
+        console.error(error);
+      }
+  }
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="relative flex flex-col items-center justify-between p-24">
+      <Dialog open={dialogOpen} onOpenChange={() => setDialog(!dialogOpen)} />
+      <div className="z-[1] relative max-h-[500px] flex flex-col w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <div className='p-5 sticky top-0 bg-black w-full flex align-middle justify-center z-10'>
+          <button onClick={() => setDialog(!dialogOpen)} className='sticky top-0 w-[200px] rounded-md bg-red-500 p-2 text-base'>Tambah User</button>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className='relative h-[350px] w-fit overflow-y-scroll rounded-md'>
+          <table className='shadow-sm shadow-slate-100 bg-amber-400 text-zinc-900'>
+            <thead className='sticky top-0'>
+              <tr className='bg-amber-400'>
+                <td className={`${className_td.className} font-semibold`}>nama lengkap</td>
+                <td className={`${className_td.className} font-semibold`}>username</td>
+                <td className={`${className_td.className} font-semibold`}>status</td>
+                <td className={`${className_td.className} font-semibold`}>aksi</td>
+              </tr>
+            </thead>
+            <tbody className='overflow-y-scroll'>
+              {data.length > 0 && data.map((i, idx) => (
+                <tr key={idx}>
+                  <td {...className_td}>iko</td>
+                  <td {...className_td}>username</td>
+                  <td {...className_td}>status</td>
+                  <td onClick={() => deleteRecord(i.userid)} className='text-right cursor-pointer hover:text-slate-200'>del</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </main>
   )
